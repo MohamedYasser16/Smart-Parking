43c2060778905e0247c28e687e33686ba0b4c58f
MohamedYasser16 <mo7medyasser16@gmail.com>
2025-07-01 03:24:42 +0300

    0122222222222

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
-import Navbar from './Component/Navbar'
 import 'flowbite' ;
 import InterHome from './Component/InterHome'
 import AccordionUsage from './Component/Acordiation'
 import Team from './Component/Team'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
 import Layout from './Component/Layout'
-import Ansys from './Component/Ansys'
 import MemberDetails from './Component/MemberDetails'
 
 
